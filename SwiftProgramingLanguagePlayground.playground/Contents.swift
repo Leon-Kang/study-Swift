948d08bcd8e383e2a0fe14e8579d1a8f8b6377b1
Leon Kang <leon@thenetcircle.com>
2017-03-23 17:06:32 +0800

    Adde a new swift playground

---
+++ SwiftProgramingLanguagePlayground.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// switch
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+
+let nickName: String? = nil
+let fullName: String = optionalName!
+let informalGreeting = "Hi \(nickName ?? fullName)"
+
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
